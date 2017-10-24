65a7e7a869dbe645abec52cb1a8c3c781f439d15
sgovil-rms <Sourabh.Govil@rms.com>
2017-10-23 22:31:57 -0700

    Add files via upload

---
+++ create_mon_job.ps1
@@ -0,0 +1,51 @@
+﻿#$Tenant = 'SCOR'
+#$Interval = 5 #minutes
+#$PriorityList = "Lowest+4"  # "Lowest+4"
+#$PriorityType = 'Highest'
+#$EventID = 9999
+#$LookBackInterval = 6 #hours
+#$SuppressAlertInterval = 60 #minutes
+
+#$Command = "C:\rms_monitoring\egc_job_monitoring.ps1 $Tenant $($Interval) $($PriorityList) $($PriorityType) $($EventID) $($LookBackInterval) $($SuppressAlertInterval)"
+
+$taskName = "EGC Job Monitoring"
+$task = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
+if ($task -ne $null)
+{
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false 
+}
+
+#$user = "$env:USERDOMAIN\$env:USERNAME"
+#Write-Output $user
+
+$response = Read-host "Enter your password?" -AsSecureString 
+$password=[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($response))
+
+$action = New-ScheduledTaskAction -Execute 'C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe' -Argument ' -command C:\rms_monitoring\scripts\egc_job_monitoring.ps1 -TenantName SCOR -CheckInterval 5 -JobPriorityList "Lowest+2","Lowest+4" -PriorityLabel Highest -EventID 9999 -LookBackInterval 1440 -SuppressAlertInterval 60' 
+$trigger =  New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration ([System.TimeSpan]::MaxValue)
+
+#$principal = New-ScheduledTaskPrincipal -UserId 'MS\svc-zen' -RunLevel Highest
+#-LogonType S4U -RunLevel Highest
+
+$definition = New-ScheduledTask -Action $action -Trigger $trigger -Description "Monitoring Highest Priority EGC Jobs"
+				#-Principal $principal `  
+				
+
+Register-ScheduledTask -TaskName $taskName -InputObject $definition -User 'MS\SVC-ZEN' -Password $password
+
+#Register-ScheduledTask -Action $action -User $principal -Trigger $trigger -TaskName $taskName -Description "Monitoring Highest Priority EGC Jobs"
+
+$task = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
+if ($task -ne $null)
+{
+	Write-Output "Created scheduled task: '$($task.ToString())'."
+	Write-Output "***********************************************************************************"
+	Write-Output ""
+	Write-Output "Please make sure to check-off 'Run with Highest privilege check box on the scheduler'"
+	Write-Output ""
+	Write-Output "***********************************************************************************"
+}
+else
+{
+	Write-Output "Created scheduled task: FAILED."
+}
---
+++ egc_job_monitoring.ps1
@@ -0,0 +1,103 @@
+﻿Param
+(
+  $TenantName, # Name of the Tenant
+  $CheckInterval, # Time interval in minutes
+  $JobPriorityList, # HPC Job Priority List - Lowest,Lowest+1,Lowest+2,Lowest+3,Lowest+4
+  $PriorityLabel, #Highest, Above Normal, Normal, Below Normal, Lowest
+  $EventID, # 9999, 9990, 9980
+  $LookBackInterval, # Time interval in minutes to look back in time for queued jobs
+  $SuppressAlertInterval # Time in minutes for suppressing alerts
+)
+
+$NumOfParams = 7
+Function LogWrite
+{
+   Param ([string]$logstring)
+   Add-content $Logfile -value "$(Get-Date): $($logstring)"
+}
+
+Add-PSSnapin Microsoft.HPC
+$BaseDir = "C:\RMS_Monitoring"
+$LogDir = "$($BaseDir)\Logs"
+$CacheDir = "$($BaseDir)\Cache"
+$DTG = Get-Date -format M.d.yyyy_HH.mm.ss
+$Logfile = "$LogDir\egc_mon_log_$DTG.log"
+#Create Logs Folder if does not exist
+If (!(Test-Path $LogDir))
+{
+	New-Item  -ItemType Directory -Force -Path  $LogDir 
+	LogWrite  "Logs directory [$LogDir] created!"
+}
+If (!(Test-Path $CacheDir))
+{
+	New-Item  -ItemType Directory -Force -Path  $CacheDir
+	LogWrite  "Cache directory [$CacheDir] created!"
+}
+LogWrite "Starting the EGC job monitoring"
+LogWrite "Checking the number of argument supplied: Required [$($NumOfParams)] vs. Supplied[$(($PSBoundParameters.values | Measure-Object | Select-Object -ExpandProperty Count))]"
+
+# Test to see if the script should exit..
+If ( ($PSBoundParameters.values | Measure-Object | Select-Object -ExpandProperty Count) -lt $NumOfParams)
+{
+	LogWrite "Exiting due to insufficient number of arguments supplied!"	
+	Write-Output "Exiting due to insufficient number of arguments supplied!"
+	EXIT
+}
+
+$SourceName = "EGC_Job_Monitoring"
+$source = [system.diagnostics.eventlog]::SourceExists("$($SourceName)")
+
+If($source -ne 'True') {
+    LogWrite "Creating new Windows Application source $($SourceName)"
+    [system.diagnostics.EventLog]::CreateEventSource("$($SourceName)", "Application")
+}
+
+$CacheFile = "$($CacheDir)\$($EventID).out"
+
+LogWrite "Cache file path is $($CacheFile)"
+
+#Add exception handling here and null input and default inputs
+$Start = (Get-Date).AddMinutes(-$LookBackInterval)
+$End = (Get-Date).AddMinutes(-$CheckInterval)
+
+LogWrite "Checking for queued jobs between StartDate: $($Start) & EndDate: $($End)"
+
+$HJobs = Get-HpcJob -BeginSubmitDate $Start -EndSubmitDate $End -State Queued -ErrorAction:SilentlyContinue |  Where-Object { $_.Priority -in ($JobPriorityList)}
+
+LogWrite "$($HJobs.Count) job(s) with [$($JobPriorityList)] priority found in the queue"
+
+If($HJobs.Count -ne 0){
+
+    If(Test-Path -Path $CacheFile)
+    {
+        $date = Get-Content -Path $CacheFile -ReadCount 1
+        $date = $date -as [DateTime];
+    }
+
+	LogWrite "Time stamp when last alert was raised: $($date)"
+	
+    #Raise the alert if the date is invalid or current time is greater than the time we last raised the alert 
+    If(!$date -or ($date).AddMinutes($SuppressAlertInterval) -le (Get-Date))
+    {   
+       LogWrite "Raising the event: $($EventID)"
+
+	   $Message = "Tenant $($TenantName): $($HJobs.Count) job(s) with $($PriorityLabel) priority found in the queue. Please check!)"
+			
+	   Write-EventLog -LogName "Application" -Source "$($SourceName)" -EventID "$($EventID)" -EntryType Warning -Message $Message
+       
+       LogWrite "Resetting the time stamp after raising the Windows Event"
+	   #Reset the time stamp after every time a windows event is raised!
+       (Get-Date) | Set-Content -Path $CacheFile
+    }
+    else
+    {
+        LogWrite "We can only raise an event after every $($SuppressAlertInterval) minutes. Suppressing raising an event."
+        Write-Output "We can only raise an event after every $($SuppressAlertInterval) minutes. Suppressing raising an event."
+    }    
+}
+else
+{
+     LogWrite "No $($PriorityLabel) priority job found in the queue. Resetting the time stamp."
+     Write-Output "No $($PriorityLabel) priority job found in the queue. Resetting the time stamp."
+     '' | Set-Content -Path $CacheFile
+}
